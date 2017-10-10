82a27c13ad32b200141a8f63923aac865fb63db0
Bruno <bcdbruno@gmail.com>
2017-10-09 22:02:32 -0300

    COMMIT
    TESTE

--- CloudVision/app/src/main/java/com/google/sample/cloudvision/ActSegundaTela.java
+++ CloudVision/app/src/main/java/com/google/sample/cloudvision/ActSegundaTela.java
@@ -3,8 +3,10 @@ package com.google.sample.cloudvision;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Handler;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.cloud.translate.Translate;
@@ -26,12 +28,19 @@ import android.widget.Toast;
 
 public class ActSegundaTela extends AppCompatActivity {
 
-    private static final String API_KEY = "";
+    private static final String API_KEY = "AIzaSyCSKfEVJQWOW9O7ZVgNwQRHA2oRb192N2k";
 
     private TextView txtTarget;
     private TextView txtSource;
-    private TextView txtFala;
-    private ImageButton btnSpeak;
+    private Button btnResultado;
+    private TextView txtFala01;
+    private TextView txtFala02;
+    private TextView txtFala03;
+    private ImageButton btnSpeak01;
+    private ImageButton btnSpeak02;
+    private ImageButton btnSpeak03;
+    boolean botao1=false, botao2=false, botao3=false;
+
     private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
@@ -41,46 +50,80 @@ public class ActSegundaTela extends AppCompatActivity {
 
         txtTarget = (TextView) findViewById(R.id.txtTarget);
         txtSource = (TextView) findViewById(R.id.txtSource);
-        txtFala = (TextView) findViewById(R.id.txtFala);
-        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        btnResultado = (Button) findViewById(R.id.btnResultado);
+        txtFala01 = (TextView) findViewById(R.id.txtFala01);
+        txtFala02 = (TextView) findViewById(R.id.txtFala02);
+        txtFala03 = (TextView) findViewById(R.id.txtFala03);
+        btnSpeak01 = (ImageButton) findViewById(R.id.btnSpeak01);
+        btnSpeak02 = (ImageButton) findViewById(R.id.btnSpeak02);
+        btnSpeak03 = (ImageButton) findViewById(R.id.btnSpeak03);
+
 
         Bundle bundle = getIntent().getExtras();
+        String word = "";
         if(bundle.containsKey("Name")){
-            final String word = bundle.getString("Name");
+            word = (String) bundle.getString("Name");
             txtTarget.setText(word);
             //envia para o translate api para traduzir a palavra pro portugues
-            final Handler textViewHandler = new Handler();
-
-            new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... params) {
-
-                    Translate translate = TranslateOptions.newBuilder().setApiKey(API_KEY).build().getService();
-
-                    final Translation translation = translate.translate(word, Translate.TranslateOption.targetLanguage("pt"));
-
-                    textViewHandler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            txtSource.setText(translation.getTranslatedText());
-                        }
-                    });
-                    return null;
-                }
-            }.execute();
-
         }
 
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
+        final String finalWord = word;
+        btnResultado.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"AQui 1", Toast.LENGTH_SHORT).show();
+                final Handler textViewHandler = new Handler();
+                Toast.makeText(getApplicationContext(),"AQui 2", Toast.LENGTH_SHORT).show();
+                new AsyncTask<Void, Void, Void>() {
+
+                    @Override
+
+                    protected Void doInBackground(Void... params) {
+                        Toast.makeText(getApplicationContext(),"AQui 4", Toast.LENGTH_SHORT).show();
+                        Translate translate = TranslateOptions.newBuilder().setApiKey(API_KEY).build().getService();
+                        Toast.makeText(getApplicationContext(),"AQui 5", Toast.LENGTH_SHORT).show();
+                        final Translation translation = translate.translate(finalWord, Translate.TranslateOption.targetLanguage("pt"));
+                        Toast.makeText(getApplicationContext(),"AQui 6", Toast.LENGTH_SHORT).show();
+                        textViewHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                txtSource.setText(translation.getTranslatedText());
+                            }
+                        });
+                        return null;
+                    }
+                }.execute();
+            }
+        });
+
+        btnSpeak01.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                botao1 = true;
                 promptSpeechInput();
             }
         });
 
+        btnSpeak02.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                botao2=true;
+                promptSpeechInput();
+            }
+        });
+
+        btnSpeak03.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                botao3=true;
+                promptSpeechInput();
+            }
+        });
     }
 
+    //BOTAO 01
     /**
      * Showing google speech input dialog
      * */
@@ -100,7 +143,6 @@ public class ActSegundaTela extends AppCompatActivity {
         }
     }
 
-
     /**
      * Receiving speech input
      * */
@@ -115,19 +157,43 @@ public class ActSegundaTela extends AppCompatActivity {
                 if (resultCode == RESULT_OK && null != data) {
 
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txtFala.setText(result.get(0));
-                    if(target.equals(result.get(0))){
-                        txtFala.setTextColor(Color.GREEN);
-                    } else
-                        txtFala.setTextColor(Color.RED);
+                    if(botao1){
+                        txtFala01.setText(result.get(0));
+                        botao1=false;
+                        if(target.equals(result.get(0))){
+                            AlertDialog alertDialog;
+                            alertDialog = new AlertDialog.Builder(this).create();
+                            alertDialog.setTitle("Você acertou!!");
+                            alertDialog.setMessage("Parabéns, continue treinando. Aqui está sua recompensa:");
+                            alertDialog.setIcon(R.drawable.bronze);
+                            alertDialog.show();
+                            txtFala01.setTextColor(Color.GREEN);
+                        } else
+                            txtFala01.setTextColor(Color.RED);
+                    }
+                    else
+                        if(botao2){
+                            txtFala02.setText(result.get(0));
+                            botao2 = false;
+                            if(target.equals(result.get(0))){
+                                txtFala02.setTextColor(Color.GREEN);
+                            } else
+                                txtFala02.setTextColor(Color.RED);
+                        }
+                        else
+                            if(botao3){
+                                txtFala03.setText(result.get(0));
+                                botao3 = false;
+                                if(target.equals(result.get(0))){
+                                    txtFala03.setTextColor(Color.GREEN);
+                                } else
+                                    txtFala03.setTextColor(Color.RED);
+                            }
                 }
                 break;
             }
 
         }
     }
-
-
-
 
 }
